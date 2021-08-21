67e4388e99f8c212c2b892edde4d52064e9146c1
AndreyPootMay <spocker312@gmail.com>
2021-08-20 21:52:38 -0500

    feat: :wrench: Adding the refactoring using only one component for edit and show.

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -3,11 +3,35 @@
 namespace App\Http\Livewire;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ShowPosts extends Component
 {
+    use WithFileUploads;
+
+    /**
+     * Search
+     * @var string
+     */
     public $search;
+
+    /**
+     * The new post or the request post.
+     * @var Post
+     */
+    public $post;
+
+    public $identifier;
+
+    public $image;
+
+    /**
+     * Manage the status of the edit modal.
+     * @var bool
+     */
+    public $open_edit = false;
 
     /**
      * Sort by column
@@ -22,16 +46,38 @@ class ShowPosts extends Component
      */
     public $direction = 'desc';
 
-    protected $listeners = [
-        'render' => 'render'
+    protected $listeners = ['render'];
+
+    /**
+     * Manage the mount of the livewire component.
+     * @return  void
+     */
+    public function mount(): void
+    {
+        $this->identifier = rand();
+        $this->open_edit = false;
+        $this->post = new Post();
+    }
+
+    /**
+     * Post model validation rules
+     * @var array
+     */
+    protected array $rules = [
+        'post.title' => 'required',
+        'post.content' => 'required',
     ];
 
+    /**
+     * Render the component with data.
+     * @return mixed
+     */
     public function render()
     {
         $posts = Post::where('title', 'like', "%{$this->search}%")
             ->orWhere('content', 'like', "%{$this->search}%")
             ->orderBy($this->sort, $this->direction)
-            ->get();
+            ->paginate(10);
 
         return view('livewire.show-posts', [
             'posts' => $posts
@@ -55,5 +101,38 @@ class ShowPosts extends Component
             $this->sort = $sort;
             $this->direction = 'asc';
         }
+    }
+
+    /**
+     * Edit an post opening the modal.
+     * @param   Post  $post
+     * @return  void
+     */
+    public function edit(Post $post): void
+    {
+        $this->post = $post;
+        $this->open_edit = true;
+    }
+
+    /**
+     * Manage the update of the post model.
+     * @return mixed
+     */
+    public function update()
+    {
+        $this->validate();
+
+        if ($this->image) {
+            Storage::delete([$this->post->image]);
+
+            $this->post->image = $this->image->store('posts');
+        }
+
+        $this->post->save();
+
+        $this->reset(['open_edit', 'image']);
+        $this->identifier = rand();
+
+        $this->emit('alert', 'Post updated successfully');
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -66,25 +66,25 @@
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($posts as $post)
+                        @foreach ($posts as $item)
                             <tr>
                                 <td class="px-6 py-4">
                                     <div class="flex items-center">
                                         <div class="text-sm font-medium text-gray-900">
-                                            {{ $post->id }}
+                                            {{ $item->id }}
                                         </div>
                                     </div>
                                 </td>
                                 <td class="px-6 py-4">
-                                    <div class="text-sm text-gray-900">{{ $post->title }}</div>
+                                    <div class="text-sm text-gray-900">{{ $item->title }}</div>
                                 </td>
                                 <td class="px-6 py-4">
                                     <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full">
-                                        {{ $post->content }}
+                                        {{ $item->content }}
                                     </span>
                                 </td>
                                 <td class="px-6 py-4">
-                                    @if ($post->active)
+                                    @if ($item->active)
                                         <span
                                             class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                                             Active
@@ -97,9 +97,9 @@
                                     @endif
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                    {{-- @livewire('edit-post', ['post' => $post], key($post->id)) --}}
-                                    <a wire:click="edit({{ $post }})" class="btn btn-green">
-                                        <i class="fas fa-edit"></i>
+                                    {{-- @livewire('edit-post', ['post' => $item], key($item->id)) --}}
+                                    <a wire:click="edit({{ $item }})" class="btn btn-green">
+                                        <i class="fas fa-pencil"></i>
                                     </a>
                                 </td>
                             </tr>
@@ -113,4 +113,55 @@
             @endif
         </x-table>
     </div>
+
+    <x-jet-dialog-modal wire:model="open_edit">
+        <x-slot name="title">
+            Update post: {{ $post->title }}
+        </x-slot>
+        <x-slot name="content">
+            <div wire:loading wire:target="image"
+                class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Wait!</strong>
+                <span class="block sm:inline">The image is uploading, wait a moment...</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20">
+                        <title>Close</title>
+                        <path
+                            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                    </svg>
+                </span>
+            </div>
+
+            @if ($image)
+                <img class="mb-4" src="{{ $image->temporaryUrl() }}">
+            @else
+                <img class="mb-4" src="{{ Storage::url($post->image) }}">
+            @endif
+
+            <div class="mb-4">
+                <x-jet-label value="Título del post" />
+                <x-jet-input wire:model="post.title" type="text" class="w-full" />
+            </div>
+            <div class="mb-4">
+                <x-jet-label value="Contenido del post" />
+                <textarea wire:model="post.content" type="text" class="form-comtrol w-full" rows="6"></textarea>
+            </div>
+
+            <div class="mb-4">
+                <input type="file" wire:model="image" id="{{ $identifier }}">
+
+                <x-jet-input-error for="image" />
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open_edit', false)" class="btn btn-red">
+                Cancel
+            </x-jet-secondary-button>
+            <x-jet-secondary-button wire:click="update" wire:loading.attr="disabled"
+                class="disabled-opacity-25 btn btn-blue">
+                Save
+            </x-jet-secondary-button>
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
